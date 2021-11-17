51b562a2972fa2efe49f856a23005043cac2bf95
Smit Tailor <smi@biz-insights.com>
2021-11-17 12:11:50 +0530

    init

--- enroll/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.db.models.fields import EmailField
-from .models import User
-# Register your models here.
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'password')
